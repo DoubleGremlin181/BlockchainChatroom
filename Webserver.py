be5e1a71ccacd5ed6b8a00a6eb5b9edc2754e840
DoubleGremlin181 <khukmani@gmail.com>
2018-08-16 01:50:01 +0530

    Fixed HTTP Responses

--- Webserver.py
+++ Webserver.py
@@ -9,36 +9,37 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = [Blockchain.create_genesis_block(datetime.datetime.now())]  #TODO Make genisis not run on every boot
                                                                          #TODO Allow syncing and verifying with a different server
 
+
+def json_block(index):
+    return json.dumps({'Index': blockchain[index].index, 'Data': blockchain[index].data, 'Hash': blockchain[index].hash,
+                        'Previous_Hash': blockchain[index].previous_hash, 'Timestamp': str(blockchain[index].timestamp)})
+
+@app.route('/', methods=['GET'])
+def root():
+    response =  {'message': 'Hello World!'}
+    return jsonify(response), 200
+
 @app.route('/new_block', methods=['POST'])
 def new_block():
-    values = request.get_json()
-    required = ['text']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
-
-    blockchain.append(Blockchain.create_new_block(blockchain[-1], datetime.datetime.now(), values['text']))
-
-    response = {'message': f'Message successfully added at index: {blockchain[-1].index}'}
-    return jsonify(response), 201
+    message_data = request.args.get('text')
+    if message_data:
+        if len(message_data) < 500:
+            blockchain.append(Blockchain.create_new_block(blockchain[-1], datetime.datetime.now(), message_data))
+            response = {'message': f'Message successfully added at index: {blockchain[-1].index}'}
+            return jsonify(response), 201
+        response = {'message': 'Message is too big '}
+        return jsonify(response), 413
+    response = {'message': 'Missing values'}
+    return jsonify(response), 400
 
 @app.route('/get_block', methods=['GET'])
 def get_block():
-    values = request.get_json()
-    if 'index' in values :
-        index = values['index']
-        if index > -(len(blockchain) - 1)and index < len(blockchain):
-            response = json.dumps({'Index': blockchain[index].index, 'Data': blockchain[index].data, 'Hash': blockchain[index].hash,
-                        'Previous_Hash': blockchain[index].previous_hash, 'Timestamp': str(blockchain[index].timestamp)})
-            return response, 200
-        else:
-            response = {'message': 'Index out of bounds'}
-            return jsonify(response), 400
+    index = int(request.args.get('index'))
+    if index >= -(len(blockchain)) and index < len(blockchain):
+        return json_block(index), 200
     else:
-        response = json.dumps({'Index': blockchain[-1].index, 'Data': blockchain[-1].data, 'Hash': blockchain[-1].hash,
-                               'Previous_Hash': blockchain[-1].previous_hash, 'Timestamp': str(blockchain[-1].timestamp)})
-        return response, 200
+        response = {'message': 'Index out of bounds'}
+        return jsonify(response), 400
 
-
-
-print(json.dumps({'Index': blockchain[0].index, 'Data': blockchain[0].data, 'Hash': blockchain[0].hash,
-                  'Previous_Hash': blockchain[0].previous_hash, 'Timestamp': str(blockchain[0].timestamp)}))
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)  #TODO Remove
